65521519a1aecdfc2afc742ac32b213f95c45d24
Rogerio <rrgmax@bol.com.br>
2019-02-08 02:50:31 -0200

    Create third test with 5 as parameter and expected 'ho ho ho ho ho' as retun

--- noel.py
+++ noel.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
 
-def solution(num):
-    if num == 1:
-        return 'Ho!'
-    return 'Ho Ho!'
+import unittest
+from noel import solution
+
+class TestNoel(unittest.TestCase):
+    def test_one_Ho(self):
+        self.assertEqual(solution(1), "Ho!")
+
+    def test_two_Ho(self):
+        self.assertEqual(solution(2), "Ho Ho!")
+    
+    def test_five_Ho(self):
+        self.assertEqual(solution(5), "Ho Ho Ho Ho Ho!")
+
+unittest.main()
 
